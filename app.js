ed01ce71de98f3685130851d9345a18868842d8a
vij_kes <97831607+vijkes@users.noreply.github.com>
2025-01-09 23:31:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-// Import the required module
-const http = require('http');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Define the port and hostname
-const PORT = 3000;
-const HOSTNAME = '127.0.0.1';
-
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response headers
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Send the response message
-  res.end('Hello from Microservice');
+app.get('/', (req, res) => {
+  res.send('Hello from Microservice');
 });
 
-// Start the server
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+app.listen(PORT, () => {
+  console.log(`Microservice is running on http://localhost:${PORT}`);
 });
