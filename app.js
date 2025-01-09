19a509ec1c14bfc8460f4ab51529be538f6f0d8d
vij_kes <97831607+vijkes@users.noreply.github.com>
2025-01-09 23:33:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Microservice');
 });
 
 app.listen(PORT, () => {
-  console.log(`Microservice is running on http://localhost:${PORT}`);
+  console.log(`Microservice running on http://localhost:${PORT}`);
 });
